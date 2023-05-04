5c8e264b258295f6651caaa7b9db949b25f1183f
tawfi5565 <132527064+tawfi5565@users.noreply.github.com>
2023-05-04 04:34:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID", "7452578"))
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")
-BOT_TOKEN = getenv("BOT_TOKEN", "5385370603:AAHjSbZFL1OLl_uOm3v8lS0iKSYhRJS_bKs")
-SESSION_NAME = getenv("SESSION_NAME", "AgAGFRW8AN9biZhAIB_X7rubbTCwNtc48oa-geXlaVsQbV9M1emAEV_iGPw_W-lNP1HGbOuQgF9NAUIxlp8-6rgN16-7Upuhst2wCxewyx0VP7Cxb5k_9EXap6_xeU2v4So9RZMt95XYxhGc4hWO_ikd9P46iHLMjXW8GjQAnGvY-bIrM6XqIXF1yeC3vixEa8knOHGkedZU3FYyLhEILy8TV6eH5jOGvsyw5o7xFElb2zZId_vlYO--B3xSWIMUR6zyH8wozehe9tIn0jZptYjY1rqWOMoYS9wtqvBThVazL60nrgL-KdikJC9beDJLLR0C-wzuAtARPCZ-JK6oG-gVAAAAAUM3evYA")
+API_ID = int(getenv("API_ID", "24284247"))
+API_HASH = getenv("API_HASH", "0df90bc85e18922bf38e5c0b6f0f22c9)
+BOT_TOKEN = getenv("BOT_TOKEN", "6293970196:AAH8JfxIuBkBFWm0--lykf6FFFqDLm4v6jI")
+SESSION_NAME = getenv("SESSION_NAME", "1BJWap1wBuydfoDbRj0lUuYOSdfWBfNOeBajgROxqJieB5zHZw5wsanPbC8JQqzC_p8_Jj5QJ20vVseQmAAetO0rd359KjiYHw1LRqNsMKrzY7QvqbtxbdWOskRXuTmtfGMSUj_AqKaNC_rWlrGM1zSSbsH4MiYFW_TMnR9NHO770TA6Fwiu1S8HLYxPtO5pEFV0_SDAbm5vOOaMXqEB6pmMYLFM7UM3JTXUv7uMuvbnV5zW1WDah4crRT3rjI-WBVwR6dFeUA9y1qPOkRIRE5rrhH7s1ikBq-OAT0J5_AaGomKvweFujOTNa_JmNwEDYsfnJ7yfhY7Tz8vq1aignG1kSRHdNLrQ=)
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "z_p_p_y")
 ALIVE_NAME = getenv("ALIVE_NAME", "song")
-BOT_USERNAME = getenv("BOT_USERNAME", "W7MBoT")
+BOT_USERNAME = getenv("BOT_USERNAME", "t_t_e_e_t_t_o_obot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/STKR2/2004")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "z_k_p_y")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL"," GU_NR")
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
